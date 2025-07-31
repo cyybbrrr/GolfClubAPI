fc081be019cd524b23e12ddb255205a0b7e01e66
cybrrgrl <contactkylebudgell@gmail.com>
2025-07-31 11:06:39 -0230

    Added main application file.

---
+++ src/main/java/com/example/golfclub/GolfClubApiApplication.java
@@ -0,0 +1,11 @@
+package com.example.golfclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfClubApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GolfClubApiApplication.class, args);
+    }
+}
